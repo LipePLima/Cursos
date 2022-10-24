3c10a037ad91082b7a9da393b0c66a1407c5117b
Felipe Lima <felipe.lima0160@gmail.com>
2022-10-24 14:41:11 -0300

    Exercícios

---
+++ Exercícios JS/Objetos/Exibir-informações.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Felipe",
+    idade: 20,
+    cpf: 19169147728,
+    email: "felipeffuzileiro@gmail.com",
+}
+
+console.log(`Usando Objetos, podemos armazenar e exibir diversos dados correlacionados do cliente. O nome dele é ${cliente.nome}, e a sua idade é ${cliente.idade}`)
