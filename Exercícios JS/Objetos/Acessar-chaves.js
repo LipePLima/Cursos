32aa12125a78c6716b4b3e1d2b6cb0ef48e2dc26
Felipe Lima <felipe.lima0160@gmail.com>
2022-10-24 18:54:19 -0300

    Exercícios

---
+++ Exercícios JS/Objetos/Acessar-chaves.js
@@ -0,0 +1,10 @@
+const clientes = {
+    nome: "Felipe",
+    idade: 20,
+    cpf: "15387642220",
+    email: "felipe@gemail.com",
+}
+
+const chaves = ['nome', 'idade', 'cpf', 'email']
+
+console.log(cliente[chaves])
---
+++ Exercícios JS/Objetos/adicionar-campo.js
@@ -0,0 +1,17 @@
+const clientes = {
+    nome: "Felipe",
+    idade: 20,
+    cpf: "15387642220",
+    email: "felipe@gemail.com",
+}
+
+console.log(clientes);
+
+// Para adicionar uma informação no objeto, é muito simples. Basta chamar o "objeto" + "." + "nome da variavel" = "informação".
+clientes.fone = '974622658'
+
+console.log(clientes)
+
+clientes.fone = '975813543'
+
+console.log(clientes)
