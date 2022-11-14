5ebb618ff319f557697ed57d1f9e50eefb0efa3c
Felipe Lima <felipe.lima0160@gmail.com>
2022-11-14 15:25:52 -0300

    Criando botão de excluir elemento

--- Alura/mochila-de-viagem/js/main.js
+++ Alura/mochila-de-viagem/js/main.js
@@ -1,26 +1,30 @@
-const form  = document.querySelector('#novoItem');
-const lista = document.querySelector('#lista');
-const itens = JSON.parse(localStorage.getItem("items")) || [];
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 itens.forEach( (elemento) => {
     criaElemento(elemento)
-});
+} )
 
-form.addEventListener('submit', (evento) => {
-    evento.preventDefault();
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
 
-    const nome       = evento.target.elements['nome'].value;
-    const quantidade = evento.target.elements['quantidade'].value;
-    const existe     = itens.find( elemento => elemento.nome == nome.value )
-    const itemAtual  = {
-        'nome': nome.value,
-        'quantidade': quantidade.value
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
 
     if (existe) {
         itemAtual.id = existe.id
         
         atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
     } else {
         itemAtual.id = itens.length
 
@@ -28,25 +32,43 @@ form.addEventListener('submit', (evento) => {
 
         itens.push(itemAtual)
     }
-    
-    itens.push(itemAtual);
-    
-    localStorage.setItem("items", JSON.stringify(itens))
-    
-    criaElemento(nome, quantidade);
 
-    form.reset()
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(item) {
-    const novoItem = document.createElement('li');
-    const numItem  = document.createElement('strong');
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
 
-    novoItem.classList.add('item');
-
-    novoItem.innerHTML += nome;
-    numItem.innerHTML = quantidade;
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
+    
+    novoItem.innerHTML += item.nome
+    novoItem.appendChild(botaoDeleta())
 
     lista.appendChild(novoItem)
-    novoItem.appendChild(numItem);
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta () {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerHTML = 'x';
+
+    elementoBotao.addEventListener('click', function () {
+        deletaElemento(this.parentNode);
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag) {
+    tag.remove()
 }
